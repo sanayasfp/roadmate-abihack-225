209edeeaf40320e537d5f0a81fbf56b909ee697b
ousmanekone225 <o.kone@magmasend.com>
2025-07-19 17:36:41 +0000

    feat: Add ProjectCard component and ProjectPage for displaying job listings

--- app/components/CoLearningHeader.tsx
+++ app/components/CoLearningHeader.tsx
@@ -2,25 +2,24 @@
 import React from "react";
 import Link from "next/link";
 import StatsSection from "./StatsSection";
+import { Container } from "./Container";
 
 const CoLearningHeader: React.FC = () => {
   return (
-    <section className="bg-[#f9f8f3] p-1 rounded-lg shadow-md max-w-full mx-auto flex flex-col md:flex-row  justify-between space-y-6 md:space-y-0">
-      <div className="space-y-4 max-w-xl px-4 md:px-0 mt-4">
-        <h1 className="text-4xl font-semibold text-black md:mt-6">
-          Découvre ta voie dans la <span className="text-orange-600">tech</span>.
-        </h1>
-        <p>
-          Passe un test de personnalité pour mieux te connaître, trouve des
-          co-apprenants qui te ressemblent, et explore les carrières tech faites
-          pour toi.
-        </p>
-      </div>
-
-      <div className="flex flex-col items-center md:items-end space-y-2">
-        <StatsSection />
-      </div>
-    </section>
+    <section className="p-1 max-w-full mx-auto flex flex-col md:flex-row space-y-6 md:space-y-0">
+        <Container>
+            <div className="space-y-4 max-w-xl px-4 md:px-0 mt-4">
+                <h1 className="text-4xl font-semibold text-black md:mt-6">
+                Découvre ta voie dans la <span className="text-orange-600">tech</span>.
+                </h1>
+                <p>
+                Passe un test de personnalité pour mieux te connaître, trouve des
+                co-apprenants qui te ressemblent, et explore les carrières tech faites
+                pour toi.
+                </p>
+            </div>
+    </Container>
+  </section>
   );
 };
 
